bf8cdd1bbe0aefe1a31bf4098ba1e5d43f7b3825
24DP2JGala <A240421JG@rvt.lv>
2026-03-04 14:54:10 +0200

    Nepabeigts 6.2

--- src/main/java/rvt/MainProgram.java
+++ src/main/java/rvt/MainProgram.java
@@ -4,12 +4,17 @@ import java.util.*;
 public class MainProgram {
     public static void main(String[] args) {
     
-        Scanner scanner = new Scanner(System.in);
-        TodoList list = new TodoList();
+        //Scanner scanner = new Scanner(System.in);
+        //TodoList list = new TodoList();
 
-        UserInterface interFace = new UserInterface(list, scanner);
+        //UserInterface interFace = new UserInterface(list, scanner);
 
-        interFace.start();
+        //interFace.start();
+
+        TodoList todooo = new TodoList();
+
+        todooo.remove(2);
+        //todooo.add("newTasknewTask");
         
         
     }
--- src/main/java/rvt/TodoList.java
+++ src/main/java/rvt/TodoList.java
@@ -1,16 +1,101 @@
 package rvt;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
+import java.nio.file.Paths;
 
 public class TodoList {
-    ArrayList<String> list = new ArrayList<>();
+    private ArrayList<String> list;
+    private final String filePath = "src/main/java/rvt/todo.csv";
     
     public TodoList() {
+        this.list = new ArrayList<>();
+        loadFromFile();
 
     }
 
+    // PRIVATE metodes: >
+
+    private void loadFromFile() {
+        try (Scanner reader = new Scanner(Paths.get(filePath))) {
+
+            while (reader.hasNextLine()) {
+                list.add(reader.nextLine());
+
+            }
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+
+    private int getLastId() {
+        int vertiba = 0;
+        try  (Scanner reader = new Scanner(Paths.get(filePath))) {
+
+            reader.nextLine();
+            while (reader.hasNextLine()) {
+                vertiba++;
+                reader.nextLine();
+
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+
+        String x = list.get(vertiba);
+        String[] masivsX = x.split(",");
+
+        return Integer.valueOf(masivsX[0]);
+
+    }
+
+    private void updateFile() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            String x;
+            String[] masivsX;
+
+            for (int i = 0; i < list.size(); i++) {
+                x = list.get(i);
+                if (i == 0) {
+                    bw.write(x);
+                    bw.newLine();
+
+                } else {
+                    masivsX = x.split(",");
+
+                    bw.write(i + "," + masivsX[1]);
+                    if (i == list.size() - 1) break;
+                    bw.newLine();
+
+                }
+
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+
+    // PUBLIC metodes: >
+
     public void add(String task) {
-        list.add(task);
+        this.list.add(task);
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(filePath), true))) {
+            bw.newLine();
+            bw.write(getLastId() +1 + "," + task);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
 
     }
 
@@ -26,7 +111,8 @@ public class TodoList {
     }
 
     public void remove(int number) {
-        list.remove(--number);
+        list.remove(number);
+        updateFile();
 
     }
 
